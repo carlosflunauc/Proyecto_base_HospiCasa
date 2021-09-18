067f4916d801ca3d46efddb360d0594339a03832
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-09-18 09:45:21 -0500

    Initial commit

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+    class Program
+    {
+        
+        private static IRepositorioPaciente _repoPaciente= new RepositorioPaciente(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World EF!");
+            //AddPaciente();
+            BuscarPaciente(1);
+            //AsignarMedico();
+        }
+
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Carlos",
+                Apellidos = "Luna",
+                NumeroTelefono = "310396",
+                Genero = Genero.Masculino,
+                Direccion = "Calle 5 No 6-7",
+                Longitud = 5.07062F,
+                Latitud = -75.52290F,
+                Ciudad = "Manizales",
+                FechaNacimiento = new DateTime(1985, 05, 19)
+            };
+            _repoPaciente.AddPaciente(paciente);
+          
+        }
+
+        private static void BuscarPaciente(int idPaciente)
+        {
+            var paciente = _repoPaciente.GetPaciente(idPaciente);
+            Console.WriteLine(paciente.Nombre+" "+paciente.Apellidos);
+        }
+
+        private static void AsignarMedico()
+        {
+            var medico = _repoPaciente.AsignarMedico(1,2);
+            Console.WriteLine(medico.Nombre+" "+medico.Apellidos);
+        }
+    }
+}
---
+++ HospiEnCasa.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Enfermera> Enfermeras { get; set; }
+        public DbSet<FamiliarDesignado> FamiliaresDesignados { get; set; }
+        public DbSet<SignoVital> SignosVitales { get; set; }
+        public DbSet<Historia> Historias { get; set; }
+        public DbSet<SugerenciaCuidado> SugerenciasCuidado { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+ .UseSqlServer("Initial Catalog=HospiEnCasaData; Data Source=HOMELUNA; Integrated Security=true");
+               //.UseSqlServer("Data Source = (localdb)\\HOMELUNA; Initial Catalog =HospiEncasatData");            
+             }
+        }
+
+    }
+}
