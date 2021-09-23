4fe60ec86330df1dd5e5baf2e0632cc15b8b100b
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-09-22 23:49:44 -0500

    ok

--- HospiEnCasa.App.Persistencia/AppRepositorios/RepositorioSaludosMemoria.cs
+++ HospiEnCasa.App.Persistencia/AppRepositorios/RepositorioSaludosMemoria.cs
@@ -3,17 +3,8 @@ using HospiEnCasa.App.Dominio;
 
 namespace HospiEnCasa.App.Persistencia.AppRepositorios
 {
-    public class RepositorioSaludosMemoria : IRepositorioSaludos
+    public class RepositorioSaludosMemoria : 
     {
-        List<Saludo> saludos;
-
-        public RepositorioSaludosMemoria()
-        {
-            saludos= new List<Saludo>()
-            {
-                new Saludo{Id=11, EnEspanol"Buenos Dias",EnIngles="Good Morning",EnItaliano="Buon giorno"}
-            }
-        }
         public IEnumerable<Saludo> GewAll()
         {
             throw new System.NotImplementedException();
