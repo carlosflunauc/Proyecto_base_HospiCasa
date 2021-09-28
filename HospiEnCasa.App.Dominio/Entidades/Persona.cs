221c197b19168ade12f448a8ba0ab4da1171e030
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-09-28 07:49:54 -0500

    crud pacientes
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- HospiEnCasa.App.Dominio/Entidades/Persona.cs
+++ HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -1,17 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 namespace HospiEnCasa.App.Dominio
 {
-     /// <summary>Class <c>Persona</c>
-     /// Modela una Persona en general en el sistema 
-     /// </summary>   
     public class Persona
     {
-        // Identificador único de cada persona
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
+       // [Display(Name = "Numero telefonico")]
         public string NumeroTelefono { get; set; }
-        // Genero de la persona
         public Genero Genero { set; get; }
 
     }
 }
+
