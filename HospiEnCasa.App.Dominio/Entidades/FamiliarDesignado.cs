6003ce0d4440e07673b4d081cb5ffd546652fb3f
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-09-28 19:08:37 -0500

    inico explicacion grupo6
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- HospiEnCasa.App.Dominio/Entidades/FamiliarDesignado.cs
+++ HospiEnCasa.App.Dominio/Entidades/FamiliarDesignado.cs
@@ -5,7 +5,6 @@ namespace HospiEnCasa.App.Dominio
     /// </summary>   
     public class FamiliarDesignado : Persona
     {
-
         public string Parentesco { get; set; }
         // Dirección de correo electrónico del FamiliarDesignado
         public string Correo { get; set; }
