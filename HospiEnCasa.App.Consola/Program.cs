6744111d6562e0471a4f4df23eb8a2c9fc29a96c
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-09-30 07:37:18 -0500

    nuevos metodos en consola
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- HospiEnCasa.App.Consola/Program.cs
+++ HospiEnCasa.App.Consola/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using HospiEnCasa.App.Dominio;
 using HospiEnCasa.App.Persistencia;
+using System.Collections.Generic;
 
 namespace HospiEnCasa.App.Consola
 {
     class Program
     {
-        
         private static IRepositorioPaciente _repoPaciente= new RepositorioPaciente(new Persistencia.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World EF!");
             //AddPaciente();
-            BuscarPaciente(2);
+            //BuscarPaciente(1);
+            //EliminarPaciente();
+
+            MostrarPacientes();
             //AsignarMedico();
         }
 
@@ -38,6 +41,19 @@ namespace HospiEnCasa.App.Consola
         {
             var paciente = _repoPaciente.GetPaciente(idPaciente);
             Console.WriteLine(paciente.Nombre+" "+paciente.Apellidos);
+        }
+        private static void EliminarPaciente(int idPaciente)
+        {
+            _repoPaciente.DeletePaciente(idPaciente);
+            Console.WriteLine("Paciente Eliminado");
+        }
+        private static void MostrarPacientes()
+        {
+            IEnumerable<Paciente> pacientes =_repoPaciente.GetAllPacientes();
+            foreach (var paciente in pacientes)
+            {
+                Console.WriteLine(paciente.Nombre+" "+paciente.Apellidos);
+            }
         }
 
         private static void AsignarMedico()
